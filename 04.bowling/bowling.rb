6a09c237df0628402fd20fc909897b8d5c44a472
Shunta-Kogen <shuntakgn@gmail.com>
2021-06-23 00:49:00 +0900

    10フレームの分割とスコア計算を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,64 +4,54 @@ score = ARGV[0]
 scores = score.split(',')
 shots = []
 scores.each do |s|
-  if s == 'X' # ストライク
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
-  end
+  shots << if s == 'X' # ストライク
+             10
+           else
+             s.to_i
+           end
 end
 
-frames = []
-# 1フレームごとにスコア配列を作る
-shots.each_slice(2) do |s|
-  frames << s
-end
-
-# ストライクのスコア配列を[10]に変換
-frames.each do |frame|
-  frame.pop(1) if frame[0] == 10
+# スコア配列を1フレームごとに分割
+frame = []
+loop do
+  frame << if shots[0] == 10
+             [shots.shift]
+           else
+             shots.shift(2)
+           end
+  break if shots[0].nil?
 end
 
 # 10フレームのスコアを1つの配列に結合
-case frames.length
+case frame.length
 when 11
-  frames[9].concat(frames[10])
-  frames.pop(1)
+  frame[9].concat(frame[10])
+  frame.pop(1)
 when 12
-  frames[9].concat(frames[10], frames[11])
-  frames.pop(2)
+  frame[9].concat(frame[10], frame[11])
+  frame.pop(2)
 end
 
 # スコア計算
 point = 0
-frames.each_with_index do |frame, i|
+frame.each_with_index do |frames, i|
   case i
-  # 1~8フレーム
-  when 0..7
-    point += if frame[0] == 10 && frames[i + 1][0] == 10 # ダブル
-               10 + 10 + frames[i + 2][0]
-             elsif frame[0] == 10 # ストライク
-               10 + frames[i + 1].sum
-             elsif frame.sum == 10 # スペア
-               10 + frames[i + 1][0]
+  # 1~9フレーム
+  when 0..8
+    point += if frames[0] == 10 && frame[9][0] == 10 && i == 8 # 9フレーム目でのダブル
+               10 + 10 + frame[9][1]
+             elsif frames[0] == 10 && frame[i + 1][0] == 10 # ダブル
+               10 + 10 + frame[i + 2][0]
+             elsif frames[0] == 10 # ストライク
+               10 + frame[i + 1][0] + frame[i + 1][1]
+             elsif frames.sum == 10 # スペア
+               10 + frame[i + 1][0]
              else
-               frame.sum
-             end
-  # 9フレーム
-  when 8
-    point += if frame[0] == 10 && frames[9][0] == 10 # ダブル
-               10 + 10 + frames[9][1]
-             elsif frame[0] == 10 # ストライク
-               10 + frames[9][0] + frames[9][1]
-             elsif frame.sum == 10 # スペア
-               10 + frames[9][0]
-             else
-               frame.sum
+               frames.sum
              end
   # 10フレーム
   when 9
-    point += frame.sum
+    point += frames.sum
   end
 end
 
