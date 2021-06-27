e1b408955428c524f75b4bbdbe6858d56d54aefe
Shunta-Kogen <shuntakgn@gmail.com>
2021-06-27 16:07:18 +0900

    shotsとframesに格納する際のif文を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,24 +2,14 @@
 
 score = ARGV[0]
 scores = score.split(',')
-shots = []
-scores.each do |s|
-  shots << if s == 'X' # ストライク
-             10
-           else
-             s.to_i
-           end
-end
+STRIKE = 10
 
-# スコア配列を10フレームに分割
+shots = scores.map { |s| s == 'X' ? STRIKE : s.to_i }
+
+# 10フレームに分割
 frames = []
 10.times do |i|
-  frames << if shots[0] == 10
-              [shots.shift]
-            else
-              shots.shift(2)
-            end
-
+  shots.first == STRIKE ? frames << [shots.shift] : frames << shots.shift(2)
   frames[-1].concat(shots) if i == 9
 end
 
@@ -29,14 +19,14 @@ frames.each_with_index do |frame, i|
   case i
   # 1~9フレーム
   when 0..8
-    point += if frame[0] == 10 && frames[i + 1][0] == 10 && i == 8 # 9フレーム目でのダブル
-               10 + 10 + frames[i + 1][1]
-             elsif frame[0] == 10 && frames[i + 1][0] == 10 # ダブル
-               10 + 10 + frames[i + 2][0]
-             elsif frame[0] == 10 # ストライク
-               10 + frames[i + 1][0] + frames[i + 1][1]
-             elsif frame.sum == 10 # スペア
-               10 + frames[i + 1][0]
+    point += if frame[0] == STRIKE && frames[i + 1][0] == STRIKE && i == 8 # 9フレーム目でのダブル
+               STRIKE + STRIKE + frames[i + 1][1]
+             elsif frame[0] == STRIKE && frames[i + 1][0] == STRIKE # ダブル
+               STRIKE + STRIKE + frames[i + 2][0]
+             elsif frame[0] == STRIKE # ストライク
+               STRIKE + frames[i + 1][0] + frames[i + 1][1]
+             elsif frame.sum == STRIKE # スペア
+               STRIKE + frames[i + 1][0]
              else
                frame.sum
              end
@@ -47,3 +37,4 @@ frames.each_with_index do |frame, i|
 end
 
 p point
+
