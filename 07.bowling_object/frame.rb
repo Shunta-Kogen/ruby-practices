1d102cd2b9f401bbd3cfa3f0688fb1bd2baeb78c
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-03 23:33:22 +0900

    third_shotを非公開、クラス内部からのアクセスはインスタンス変数を参照するように変更、initializeの引数をmarksに変更

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,21 +3,20 @@
 require './shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot
+  attr_reader :first_shot, :second_shot
 
-  def initialize(frame)
-    @frame = frame
-    @first_shot = Shot.new(@frame[0]).score
-    @second_shot = Shot.new(@frame[1]).score
-    @third_shot = Shot.new(@frame[2]).score
+  def initialize(marks)
+    @first_shot = Shot.new(marks[0]).score
+    @second_shot = Shot.new(marks[1]).score
+    @third_shot = Shot.new(marks[2]).score
   end
 
   def score
-    [first_shot, second_shot, third_shot].sum
+    [@first_shot, @second_shot, @third_shot].sum
   end
 
   def strike?
-    first_shot == 10
+    @first_shot == 10
   end
 
   def spare?
