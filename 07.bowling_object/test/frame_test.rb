519fc088986cf971fbd63fe140acd8cd9e30854c
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-03 13:46:36 +0900

    frameテストのテストメソッドを変更

--- 07.bowling_object/test/frame_test.rb
+++ 07.bowling_object/test/frame_test.rb
@@ -4,51 +4,21 @@ require 'minitest/autorun'
 require './frame'
 
 class FrameTest < Minitest::Test
-  def test_frame00
-    frame = [0, 0]
-    frame_test = Frame.new(frame)
-    assert_equal 0, frame_test.score
+  def test_score
+    assert_equal 0, Frame.new([0, 0]).score
+    assert_equal 10, Frame.new([5, 5]).score
+    assert_equal 9, Frame.new([5, 4]).score
   end
 
-  def test_frame54
-    frame = [5, 4]
-    frame_test = Frame.new(frame)
-    assert_equal 9, frame_test.score
+  def test_strike
+    assert Frame.new([10]).strike?
+    refute Frame.new([5, 5]).strike?
+    refute Frame.new([5, 4]).strike?
   end
 
-  def test_strike10
-    frame = [10]
-    frame_test = Frame.new(frame)
-    assert frame_test.strike?
-  end
-
-  def test_strike55
-    frame = [5, 5]
-    frame_test = Frame.new(frame)
-    refute frame_test.strike?
-  end
-
-  def test_strike54
-    frame = [5, 4]
-    frame_test = Frame.new(frame)
-    refute frame_test.strike?
-  end
-
-  def test_spare10
-    frame = [10]
-    frame_test = Frame.new(frame)
-    refute frame_test.spare?
-  end
-
-  def test_spare55
-    frame = [5, 5]
-    frame_test = Frame.new(frame)
-    assert frame_test.spare?
-  end
-
-  def test_spare54
-    frame = [5, 4]
-    frame_test = Frame.new(frame)
-    refute frame_test.spare?
+  def test_spare
+    refute Frame.new([10]).spare?
+    assert Frame.new([5, 5]).spare?
+    refute Frame.new([5, 4]).spare?
   end
 end
