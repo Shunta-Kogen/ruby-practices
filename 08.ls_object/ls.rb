5163d17f46880ca43a0f17db33e304dec8015d07
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-09 15:55:28 +0900

    require周りを修正、initializeにfile_dataを追加

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-require './option'
-require './filedata'
-require './short_format'
-require './long_format'
+require 'optparse'
+require_relative 'option'
+require_relative 'short_format'
+require_relative 'long_format'
 
 class Ls
   def initialize(option)
     @option = Option.new(option)
-    @short_format = ShortFormat.new(option)
-    @long_format = LongFormat.new(option)
+    @file_data = @option.a_option? ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    @file_data.reverse! if @option.r_option?
+    @short_format = ShortFormat.new(@option, @file_data)
+    @long_format = LongFormat.new(@option, @file_data)
   end
 
   def output
