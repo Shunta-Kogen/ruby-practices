1db76d60c6b1488f83dcb48b520bf029f55bbe99
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-10 16:31:20 +0900

    変数名を変更、short_format.rb,long_format.rbの共通部分をls.rbで実行するよう修正

--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -2,16 +2,20 @@
 
 require 'optparse'
 require_relative 'option'
+require_relative 'file_data'
 require_relative 'short_format'
 require_relative 'long_format'
 
 class Ls
   def initialize(option)
     @option = Option.new(option)
-    @file_data = @option.a_option? ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-    @file_data.reverse! if @option.r_option?
-    @short_format = ShortFormat.new(@option, @file_data)
-    @long_format = LongFormat.new(@option, @file_data)
+    @file_names = @option.a_option? ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    @file_names.reverse! if @option.r_option?
+    @file_data_list = @file_names.map do |file|
+      FileData.new(file)
+    end
+    @short_format = ShortFormat.new(@option, @file_data_list)
+    @long_format = LongFormat.new(@option,@file_data_list)
   end
 
   def output
