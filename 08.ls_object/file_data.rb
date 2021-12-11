2c06c0d13a378cbd13ca1a3787a778a922d6e11a
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-12 00:35:08 +0900

    ファイルタイプとパーミッションのハッシュ表を定数化、filemodeメソッド内で直接ファイルモードを取得するように変更

--- 08.ls_object/file_data.rb
+++ 08.ls_object/file_data.rb
@@ -6,68 +6,64 @@ class FileData
   attr_reader :name
 
   def initialize(file)
-    @file = File::Stat.new(file)
+    @file_stat = File::Stat.new(file)
     @name = file
   end
 
   def blocks
-    @file.blocks
+    @file_stat.blocks
   end
 
+  FILETYPE_TABLE = {
+    '01': 'p',
+    '02': 'c',
+    '04': 'd',
+    '06': 'b',
+    '10': '-',
+    '12': 'l',
+    '14': 's'
+  }
+
+  PERMISSION_TABLE = {
+    '0': '---',
+    '1': '--x',
+    '2': '-w-',
+    '3': '-wx',
+    '4': 'r--',
+    '5': 'r-x',
+    '6': 'rw-',
+    '7': 'rwx'
+  }
+
   def filemode
-    filemode_number = @file.mode.to_s(8).rjust(6, '0')
+    filemode_number = @file_stat.mode.to_s(8).rjust(6, '0')
 
     filetype_number = filemode_number[0..1]
     permission_number = filemode_number[3..5]
 
-    filemode = get_filetype(filetype_number) +
-               get_permission(permission_number[0]) +
-               get_permission(permission_number[1]) +
-               get_permission(permission_number[2])
-  end
-
-  def get_filetype(filetype_number)
-    {
-      '01': 'p',
-      '02': 'c',
-      '04': 'd',
-      '06': 'b',
-      '10': '-',
-      '12': 'l',
-      '14': 's'
-    }[filetype_number.to_sym]
-  end
-
-  def get_permission(permission_number)
-    {
-      '0': '---',
-      '1': '--x',
-      '2': '-w-',
-      '3': '-wx',
-      '4': 'r--',
-      '5': 'r-x',
-      '6': 'rw-',
-      '7': 'rwx'
-    }[permission_number.to_sym]
+    filemode = FILETYPE_TABLE[filetype_number.to_sym] +
+              PERMISSION_TABLE[permission_number[0].to_sym] +
+              PERMISSION_TABLE[permission_number[1].to_sym] +
+              PERMISSION_TABLE[permission_number[2].to_sym]
   end
 
   def hardlink
-    @file.nlink
+    @file_stat.nlink
   end
 
   def owner
-    Etc.getpwuid(@file.uid).name
+    Etc.getpwuid(@file_stat.uid).name
   end
 
   def group
-    Etc.getgrgid(@file.gid).name
+    Etc.getgrgid(@file_stat.gid).name
   end
 
   def filesize
-    @file.size
+    @file_stat.size
   end
 
   def mtime
-    @file.mtime
+    @file_stat.mtime
   end
 end
