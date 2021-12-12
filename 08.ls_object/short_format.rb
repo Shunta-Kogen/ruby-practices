f49b7366cc3d7059d214dc6ce5b4e951093af7d9
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-12 14:21:57 +0900

    outputメソッドの変数名を変更

--- 08.ls_object/short_format.rb
+++ 08.ls_object/short_format.rb
@@ -10,20 +10,20 @@ class ShortFormat
   end
 
   def output
-    name_list = @file_data_list.map {|file_data| file_data.name}
+    filenames = @file_data_list.map {|file_data| file_data.name}
 
-    split_name_list = name_list.each_slice((name_list.length / COLUMNS).ceil).to_a
+    split_filenames = filenames.each_slice((filenames.length / COLUMNS).ceil).to_a
 
-    (split_name_list.first.length - split_name_list.last.length).times do
-      split_name_list.last << ' '
+    (split_filenames.first.length - split_filenames.last.length).times do
+      split_filenames.last << ' '
     end
 
-    transposed_split_name_list = split_name_list.transpose
-    longest_name_length = name_list.map(&:length).max
+    transposed_split_filenames = split_filenames.transpose
+    longest_name_length = filenames.map(&:length).max
 
-    transposed_split_name_list.each do |transposed_split_names|
-      transposed_split_names.each do |transposed_split_name|
-        print transposed_split_name.ljust(longest_name_length + COLUMN_SPACE, ' ')
+    transposed_split_filenames.each do |transposed_split_filename|
+      transposed_split_filename.each do |transposed_split_filename_element|
+        print transposed_split_filename_element.ljust(longest_name_length + COLUMN_SPACE, ' ')
       end
       print "\n"
     end
