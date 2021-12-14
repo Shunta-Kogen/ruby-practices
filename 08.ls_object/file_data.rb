e3124daef57b0981e0683a29a8b6894db2921c4c
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-15 01:15:32 +0900

    ハッシュ表のキーをシンボルではなく文字列に変更

--- 08.ls_object/file_data.rb
+++ 08.ls_object/file_data.rb
@@ -3,6 +3,27 @@
 require 'etc'
 
 class FileData
+  FILETYPE_TABLE = {
+    '01' => 'p',
+    '02' => 'c',
+    '04' => 'd',
+    '06' => 'b',
+    '10' => '-',
+    '12' => 'l',
+    '14' => 's'
+  }
+
+  PERMISSION_TABLE = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }
+
   attr_reader :name
 
   def initialize(file)
@@ -14,37 +35,16 @@ class FileData
     @file_stat.blocks
   end
 
-  FILETYPE_TABLE = {
-    '01': 'p',
-    '02': 'c',
-    '04': 'd',
-    '06': 'b',
-    '10': '-',
-    '12': 'l',
-    '14': 's'
-  }
-
-  PERMISSION_TABLE = {
-    '0': '---',
-    '1': '--x',
-    '2': '-w-',
-    '3': '-wx',
-    '4': 'r--',
-    '5': 'r-x',
-    '6': 'rw-',
-    '7': 'rwx'
-  }
-
   def filemode
     filemode_number = @file_stat.mode.to_s(8).rjust(6, '0')
 
     filetype_number = filemode_number[0..1]
     permission_number = filemode_number[3..5]
 
-    filemode = FILETYPE_TABLE[filetype_number.to_sym] +
-              PERMISSION_TABLE[permission_number[0].to_sym] +
-              PERMISSION_TABLE[permission_number[1].to_sym] +
-              PERMISSION_TABLE[permission_number[2].to_sym]
+    FILETYPE_TABLE[filetype_number] +
+    PERMISSION_TABLE[permission_number[0]] +
+    PERMISSION_TABLE[permission_number[1]] +
+    PERMISSION_TABLE[permission_number[2]]
   end
 
   def hardlink
