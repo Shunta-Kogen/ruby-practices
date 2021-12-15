ba278082891b5f629a0d7ecd3e38b61059c90168
Shunta-Kogen <shuntakgn@gmail.com>
2021-12-15 11:26:10 +0900

    time_stampメソッドを作成、total_blocksメソッドを削除し、outputメソッドで直接実行するよう修正

--- 08.ls_object/long_formatter.rb
+++ 08.ls_object/long_formatter.rb
@@ -9,23 +9,15 @@ class LongFormatter
   end
 
   def output
-    total_blocks
+    puts "total #{@file_data_list.sum(&:blocks)}"
+
     @file_data_list.each do |file_data|
       print file_data.filemode.ljust(4)
       print file_data.hardlink.to_s.rjust(3) + ' '
       print file_data.owner.ljust(10)
       print file_data.group.ljust(6)
       print file_data.filesize.to_s.rjust(5) + ' '
-
-      time_stamp = file_data.mtime
-      time_stamp_to_date = time_stamp.to_date
-      today_to_6_months_ago = Range.new(Date.today << 6, Date.today)
-      if today_to_6_months_ago.cover?(time_stamp_to_date)
-        print time_stamp.strftime('%_m %e %R').ljust(12)
-      else
-        print time_stamp.strftime('%_m %e  %Y').ljust(12)
-      end
-
+      print time_stamp(file_data.mtime).ljust(12)
       print file_data.name
       print "\n"
     end
@@ -33,7 +25,13 @@ class LongFormatter
 
   private
 
-  def total_blocks
-    puts "total #{@file_data_list.sum(&:blocks)}"
+  def time_stamp(mtime)
+    time_stamp_to_date = mtime.to_date
+    today_to_6_months_ago = Range.new(Date.today << 6, Date.today)
+    if today_to_6_months_ago.cover?(time_stamp_to_date)
+      mtime.strftime('%_m %e %R')
+    else
+      mtime.strftime('%_m %e  %Y')
+    end
   end
 end
